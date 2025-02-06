1bb526752348884fa0bbb3449ddb6ccc57f9d6c4
Shulamit-Marmerush <sm6736243@gmail.com>
2025-02-06 20:18:03 +0200

    change second

--- Program.cs
+++ Program.cs
@@ -34,15 +34,15 @@ builder.Services.AddDbContext<ToDoDbContext>(opt =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI(options =>
     {
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
         options.RoutePrefix = string.Empty; 
     });
-}
+// }
 
 
 
@@ -92,6 +92,7 @@ app.MapDelete("/items/{id}", async (int id, ToDoDbContext db) =>
     await db.SaveChangesAsync();
     return Results.NoContent();
 });
+app.MapGet("/",()=>"server api is running");
 
 
 
