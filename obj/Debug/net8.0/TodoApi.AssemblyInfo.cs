4a1322a2b275d8710f561f71410c87004a28fd8c
Shulamit-Marmerush <sm6736243@gmail.com>
2025-02-10 12:13:55 +0200

    try

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44cd4cdad3aec61be9f8c77ca26cfa98f5d54207")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48457de6d77c863fc2312f00aa821c266f51bcca")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
